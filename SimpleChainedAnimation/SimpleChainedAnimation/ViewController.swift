7bf2f17c73a8da48f6bc0004316f4706a7405fce
Antonio Posabella <antonioposabella91@gmail.com>
2019-08-06 15:31:15 +0200

    Stack View for title and body Label

--- SimpleChainedAnimation/SimpleChainedAnimation/ViewController.swift
+++ SimpleChainedAnimation/SimpleChainedAnimation/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+   private let titleLabel: UILabel = {
+        let titleLabel = UILabel()
+        titleLabel.numberOfLines = 0
+        titleLabel.text = "Lorem ipsum dolor sit amet"
+        titleLabel.font = UIFont(name: "Futura", size: 34)
+        return titleLabel
+    }()
+    
+   private let bodyLabel: UILabel = {
+        let bodyLabel = UILabel()
+        bodyLabel.numberOfLines = 0
+        bodyLabel.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        return bodyLabel
+    }()
+    
+    
+    fileprivate func setupStackView() {
+        
+        let stackView = UIStackView(arrangedSubviews: [titleLabel, bodyLabel])
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        view.addSubview(stackView)
+        // enable autolayout
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        stackView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -100).isActive = true
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupStackView()
     }
-
-
 }
 
